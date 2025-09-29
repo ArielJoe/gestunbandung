d76ab56381a7eee85e022082d19883721d164eac
Ariel Jonathan <ariel.crb01@gmail.com>
2025-09-29 16:50:52 +0700

    [fix] remove address and ui on location

--- src/app/components/Locations.tsx
+++ src/app/components/Locations.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import Image from "next/image";
+import { FaSearch } from "react-icons/fa";
 
-// Define the type for a location
 type Location = {
   name: string;
-  address: string;
   image: string;
   mapLink: string;
 };
@@ -14,53 +13,54 @@ export default function Locations() {
   const locations: Location[] = [
     {
       name: "MAZ 23 PROPERTY",
-      address: "Jl. Rajawali Barat No.19 Bandung, 40184",
       image: "/assets/maz_23_property_1.jpg",
       mapLink: "https://maps.app.goo.gl/U4Rpu3qEMwHbHtYN7",
     },
     {
       name: "CENTER PRO",
-      address: "Jl. BKR No.98 Bandung, 40254",
       image: "/assets/center_pro_1.jpg",
       mapLink: "https://maps.app.goo.gl/6TU4K1ouuRZdxRTH9",
     },
   ];
 
   return (
-    <section id="loc" className="py-10">
+    <section id="loc" className="py-10 pb-4">
       <div className="container mx-auto px-4">
-        {/* Heading */}
-        <h2 className="text-3xl md:text-4xl font-bold text-center text-primary-blue mb-12">
+        <h2 className="text-3xl md:text-4xl font-bold text-center text-primary-blue mb-10">
           Lokasi Kami
         </h2>
 
-        {/* Location Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8">
           {locations.map((location, index) => (
-            <a
+            <div
               key={index}
-              href={location.mapLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group flex flex-col h-full bg-white rounded-xl shadow-md hover:shadow-xl transition transform hover:-translate-y-1 hover:scale-[1.02]"
+              className="bg-white rounded-xl shadow-md overflow-hidden"
             >
-              <div className="relative w-full h-60">
+              <div className="relative w-full h-64 md:h-72">
                 <Image
                   src={location.image}
                   alt={location.name}
                   fill
-                  className="object-cover rounded-t-xl"
+                  className="object-cover"
                 />
               </div>
-              <div className="p-6 flex-1 flex flex-col justify-between">
-                <h3 className="text-lg md:text-xl font-semibold text-primary-blue mb-2">
+
+              <a
+                href={location.mapLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="p-4 block md:flex items-center justify-between focus:outline-none"
+                aria-label={`Lihat lokasi ${location.name} di Google Maps`}
+              >
+                <h3 className="text-lg md:text-xl font-bold text-gray-800 truncate">
                   {location.name}
                 </h3>
-                <p className="text-black text-sm md:text-base leading-relaxed">
-                  {location.address}
-                </p>
-              </div>
-            </a>
+                <span className="flex items-center text-primary-blue font-bold text-sm md:text-base">
+                  <FaSearch className="mr-2" />
+                  <span>Pelajari Lebih Lanjut</span>
+                </span>
+              </a>
+            </div>
           ))}
         </div>
       </div>
